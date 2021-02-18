6bfa4918df8a52a61b16bd7f885c6c7206ca2376
Doshna UmmaReddy <doshna.u@gmail.com>
2021-02-18 13:28:22 -0800

    Create DynamicProgramming-makeChangeBruteForce.py

---
+++ DynamicProgramming-makeChangeBruteForce.py
@@ -0,0 +1,11 @@
+def makechangeBF(coins, amount):
+    if(amount == 0): return 0
+    result = amount+1 #some arbitary huge number that cannot be the answer
+
+    for i in range(len(coins) - 1):
+        if(coins[i] <= amount):
+            result = min(result, makechangeBF(coins, amount-coins[i]) + 1)
+    return result
+
+
+print(makechangeBF([1,3,5] , 9 ))
